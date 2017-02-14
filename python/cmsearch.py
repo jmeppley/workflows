94240d807ace9e8495746cdecbdf90c0f3fd3969
John Eppley <jmeppley@gmail.com>
2017-02-14 11:02:50 -0800

    add step to read annotation owrkflow to remove rRNA reads

---
+++ python/cmsearch.py
@@ -0,0 +1,32 @@
+"""
+Suppport methods for running cmsearch
+"""
+import os
+import re
+
+def get_cm_model_version(models_dir):
+    """
+    Gets the version of the cm and hmm models used from a "VERSION"
+    file in the models folder.
+
+    Returns "Unknown" if the version file is missing.
+    """
+
+    # Look for a VERSION file
+    version_file = os.path.join(models_dir, 'VERSION')
+    if os.path.exists(version_file):
+        with open(version_file) as VF:
+            return next(VF).strip()
+
+    # Fall back to README file
+    readme_file = os.path.join(models_dir, 'README')
+    if os.path.exists(readme_file):
+        with open(readme_file) as RMF:
+            for line in RMF:
+                if re.search(r'(Release\s*[0-9.]+)', line):
+                    return "RFAM " + line.strip()
+
+    # Fallback to unknown
+    return "Unknown"
+
+
